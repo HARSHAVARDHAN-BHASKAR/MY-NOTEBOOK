bd9dd344c8eea2880cca2376b7acd316176ad8c6
HARSHAVARDHAN-BHASKAR <harshavardhanb840@gmail.com>
2025-08-09 15:43:58 +0530

    Deployment preparation

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/ratelimiter.js";
 import nodeRoute from './routes/nodeRoute.js';
@@ -10,11 +11,13 @@ connectDB();
 
 const app=express()
 const PORT=process.env.PORT || 5001;
-
+const __dirname = path.resolve()
 //cors method
-app.use(cors({
+if(process.env.NODE_ENV !== "production"){
+app.use(
+    cors({
     origin:"http://localhost:5173",
-}));
+}));}
 
 //middleware
 app.use(express.json());
@@ -29,6 +32,13 @@ app.use(rateLimiter);
 
 
 app.use("/api/notes",nodeRoute);
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../FRONTEND/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../FRONTEND","dist","index.html"))
+});
+}
 
 app.listen(PORT,()=>{
     console.log("Server started on port:", PORT )
